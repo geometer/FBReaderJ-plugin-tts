0c3c7dca7adae6c46f521eda156515a237aefc56
Nikolay Pultsin <geometer@fbreader.org>
2011-06-19 19:52:49 +0100

    API synchronization; error messages; code cleanup; version number => 1.0

--- src/org/geometerplus/fbreader/plugin/tts/SpeakActivity.java
+++ src/org/geometerplus/fbreader/plugin/tts/SpeakActivity.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2009-2011 Geometer Plus <contact@geometerplus.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
 package org.geometerplus.fbreader.plugin.tts;
 
 import java.util.HashMap;
@@ -10,6 +29,7 @@ import android.speech.tts.TextToSpeech;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.view.View;
+import android.widget.Toast;
 
 import org.geometerplus.android.fbreader.api.*;
 
@@ -18,9 +38,6 @@ public class SpeakActivity extends Activity implements TextToSpeech.OnInitListen
 
 	private static final String UTTERANCE_ID = "FBReaderTTSPlugin";
 
-	static final int CURRENTORFORWARD = 0;
-	static final int SEARCHFORWARD = 1;
-
 	private TextToSpeech myTTS;
 
 	private int myParagraphIndex = -1;
@@ -28,28 +45,8 @@ public class SpeakActivity extends Activity implements TextToSpeech.OnInitListen
 
 	private boolean myIsActive = false;
 
-	private PhoneStateListener mPhoneListener = new PhoneStateListener() {
-		public void onCallStateChanged(int state, String incomingNumber) {
-			if (state == TelephonyManager.CALL_STATE_RINGING) {
-				stopTalking();
-				finish();
-			}
-		}
-	};
-
 	private void setListener(int id, View.OnClickListener listener) {
 		findViewById(id).setOnClickListener(listener);
-	}
-
-	private void highlightParagraph() throws ApiException {
-		if (0 <= myParagraphIndex && myParagraphIndex < myParagraphsNumber) {
-			myApi.highlightArea(
-				new TextPosition(myParagraphIndex, 0, 0),
-				new TextPosition(myParagraphIndex, Integer.MAX_VALUE, 0)
-			);
-		} else {
-			myApi.clearHighlighting();
-		}
 	}
 
 	@Override
@@ -63,28 +60,16 @@ public class SpeakActivity extends Activity implements TextToSpeech.OnInitListen
 		setListener(R.id.button_previous_paragraph, new View.OnClickListener() {
 			public void onClick(View v) {
 				stopTalking();
-				try {
-					for (int i = myParagraphIndex - 1; i >= 0; --i) {
-						if (myApi.getParagraphText(i).length() > 0) {
-							myParagraphIndex = i;
-							break;
-						}
-					}
-					if (myApi.getPageStart().ParagraphIndex >= myParagraphIndex) {
-						myApi.setPageStart(new TextPosition(myParagraphIndex, 0, 0));
-					}
-					highlightParagraph();
-					findViewById(R.id.button_next_paragraph).setEnabled(true);
-					findViewById(R.id.button_play).setEnabled(true);
-				} catch (ApiException e) {
-					e.printStackTrace();
-				}
+				gotoPreviousParagraph();
 			}
 		});
 		setListener(R.id.button_next_paragraph, new View.OnClickListener() {
 			public void onClick(View v) {
 				stopTalking();
-				nextParagraphString(SEARCHFORWARD);
+				if (myParagraphIndex < myParagraphsNumber) {
+					++myParagraphIndex;
+					gotoNextParagraph();
+				}
 			}
 		});
 		setListener(R.id.button_close, new View.OnClickListener() {
@@ -101,16 +86,22 @@ public class SpeakActivity extends Activity implements TextToSpeech.OnInitListen
 		setListener(R.id.button_play, new View.OnClickListener() {
 			public void onClick(View v) {
 				setActive(true);
-				if (myIsActive) {
-					speakString(nextParagraphString(CURRENTORFORWARD));
-				}
+				speakString(gotoNextParagraph());
 			}
 		});
 
-		setActive(false);
+		((TelephonyManager)getSystemService(TELEPHONY_SERVICE)).listen(
+			new PhoneStateListener() {
+				public void onCallStateChanged(int state, String incomingNumber) {
+					if (state == TelephonyManager.CALL_STATE_RINGING) {
+						stopTalking();
+					}
+				}
+			},
+			PhoneStateListener.LISTEN_CALL_STATE
+		);
 
-		TelephonyManager tm = (TelephonyManager)getSystemService(TELEPHONY_SERVICE);
-		tm.listen(mPhoneListener, PhoneStateListener.LISTEN_CALL_STATE);
+		setActive(false);
 
 		startActivityForResult(
 			new Intent(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA), 0
@@ -118,105 +109,18 @@ public class SpeakActivity extends Activity implements TextToSpeech.OnInitListen
 	}
 
 	@Override
-	protected void onResume() {
-		myApi.connect();
-		super.onResume();
-	}
-
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
-			// success, create the TTS instance
 			myTTS = new TextToSpeech(this, this);
 		} else {
-			// missing data, install it
 			startActivity(new Intent(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA));
 		}
 	}
 
-	private void setActive(final boolean active) {
-		if (active && myParagraphIndex == -1) {
-			try {
-				myParagraphIndex = myApi.getPageStart().ParagraphIndex;
-				myParagraphsNumber = myApi.getParagraphsNumber();
-			} catch (ApiException e) {
-				e.printStackTrace();
-			}
-			if (myParagraphIndex == -1) {
-				return;
-			}
-		}
-
-		myIsActive = active;
-
-		runOnUiThread(new Runnable() {
-			public void run() {
-				findViewById(R.id.button_play).setVisibility(active ? View.GONE : View.VISIBLE);
-				findViewById(R.id.button_pause).setVisibility(active ? View.VISIBLE : View.GONE);
-			}
-		});
-	}
-
-	private void speakString(String text) {
-		setActive(true);
-
-		HashMap<String, String> callbackMap = new HashMap<String, String>();
-		callbackMap.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, UTTERANCE_ID);
-
-		myTTS.speak(text, TextToSpeech.QUEUE_FLUSH, callbackMap);
-	}
-
-	private String nextParagraphString(int direction) {
-		if (myParagraphIndex == -1) {
-			return "";
-		}
-
-		try {
-			if (direction == SEARCHFORWARD) {
-				++myParagraphIndex;
-			}
-			String text = "";
-			for (; myParagraphIndex < myParagraphsNumber; ++myParagraphIndex) {
-				final String s = myApi.getParagraphText(myParagraphIndex);
-				if (s.length() > 0) {
-					text = s;
-					break;
-				}
-			}
-			if (!myApi.isPageEndOfText()) {
-				myApi.setPageStart(new TextPosition(myParagraphIndex, 0, 0));
-			}
-			highlightParagraph();
-			if (myParagraphIndex >= myParagraphsNumber) {
-				runOnUiThread(new Runnable() {
-					public void run() {
-						findViewById(R.id.button_next_paragraph).setEnabled(false);
-						findViewById(R.id.button_play).setEnabled(false);
-					}
-				});
-			}
-			return text;
-		} catch (ApiException e) {
-			e.printStackTrace();
-			return "";
-		}
-	}
-
 	@Override
-	protected void onDestroy() {
-		if (myTTS != null) {
-			setActive(false);
-			myTTS.shutdown();
-		}
-		super.onDestroy();
-	}
-
-	private void stopTalking() {
-		setActive(false);
-		if (myTTS != null) {
-			if (myTTS.isSpeaking()) {
-				myTTS.stop();
-			}
-		}
+	protected void onResume() {
+		myApi.connect();
+		super.onResume();
 	}
 
 	@Override
@@ -237,38 +141,170 @@ public class SpeakActivity extends Activity implements TextToSpeech.OnInitListen
 	}
 
 	@Override
+	protected void onDestroy() {
+		if (myTTS != null) {
+			myTTS.shutdown();
+		}
+		super.onDestroy();
+	}
+
+	@Override
 	public void onInit(int status) {
 		myTTS.setOnUtteranceCompletedListener(this);
-		try {
-			setTitle(myApi.getBookTitle());
-		} catch (ApiException e) {
-			e.printStackTrace();
-			finish();
-			return;
-		}
 
 		try {
-			Locale locale = new Locale(myApi.getBookLanguage());
-			if (myTTS.isLanguageAvailable(locale) < 0) {
+			setTitle(myApi.getBookTitle());
+
+			Locale locale;
+			final String language = myApi.getBookLanguage();
+			if ("other".equals(language)) {
 				locale = Locale.getDefault();
+				if (myTTS.isLanguageAvailable(locale) < 0) {
+					locale = Locale.ENGLISH;
+				}
+				showMessage(
+					getText(R.string.language_is_not_set).toString()
+						.replace("%0", locale.getDisplayLanguage())
+				);
+			} else {
+				locale = new Locale(myApi.getBookLanguage());
+				if (myTTS.isLanguageAvailable(locale) < 0) {
+					final Locale originalLocale = locale;
+					locale = Locale.getDefault();
+					if (myTTS.isLanguageAvailable(locale) < 0) {
+						locale = Locale.ENGLISH;
+					}
+					showMessage(
+						getText(R.string.no_data_for_language).toString()
+							.replace("%0", originalLocale.getDisplayLanguage())
+							.replace("%1", locale.getDisplayLanguage())
+					);
+				}
 			}
 			myTTS.setLanguage(locale);
+
+			myParagraphIndex = myApi.getPageStart().ParagraphIndex;
+			myParagraphsNumber = myApi.getParagraphsNumber();
+			setActive(true);
+			speakString(gotoNextParagraph());
 		} catch (ApiException e) {
+			runOnUiThread(new Runnable() {
+				public void run() {
+					findViewById(R.id.button_previous_paragraph).setEnabled(false);
+					findViewById(R.id.button_next_paragraph).setEnabled(false);
+					findViewById(R.id.button_play).setEnabled(false);
+				}
+			});
+			showMessage(getText(R.string.initialization_error));
 			e.printStackTrace();
 		}
-		setActive(true);
-		speakString(nextParagraphString(CURRENTORFORWARD));
 	}
 
 	@Override
 	public void onUtteranceCompleted(String uttId) {
 		if (myIsActive && UTTERANCE_ID.equals(uttId)) {
-			speakString(nextParagraphString(SEARCHFORWARD));
+			++myParagraphIndex;
+			speakString(gotoNextParagraph());
 			if (myParagraphIndex >= myParagraphsNumber) {
 				stopTalking();
 			}
 		} else {
 			setActive(false);
+		}
+	}
+
+	private void highlightParagraph() throws ApiException {
+		if (0 <= myParagraphIndex && myParagraphIndex < myParagraphsNumber) {
+			myApi.highlightArea(
+				new TextPosition(myParagraphIndex, 0, 0),
+				new TextPosition(myParagraphIndex, Integer.MAX_VALUE, 0)
+			);
+		} else {
+			myApi.clearHighlighting();
+		}
+	}
+
+	private void stopTalking() {
+		setActive(false);
+		if (myTTS != null && myTTS.isSpeaking()) {
+			myTTS.stop();
+		}
+	}
+
+	private void showMessage(final CharSequence text) {
+		runOnUiThread(new Runnable() {
+			public void run() {
+				Toast.makeText(SpeakActivity.this, text, Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+
+	private void setActive(final boolean active) {
+		myIsActive = active;
+
+		runOnUiThread(new Runnable() {
+			public void run() {
+				findViewById(R.id.button_play).setVisibility(active ? View.GONE : View.VISIBLE);
+				findViewById(R.id.button_pause).setVisibility(active ? View.VISIBLE : View.GONE);
+			}
+		});
+	}
+
+	private void speakString(String text) {
+		HashMap<String, String> callbackMap = new HashMap<String, String>();
+		callbackMap.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, UTTERANCE_ID);
+		myTTS.speak(text, TextToSpeech.QUEUE_FLUSH, callbackMap);
+	}
+
+	private void gotoPreviousParagraph() {
+		try {
+			for (int i = myParagraphIndex - 1; i >= 0; --i) {
+				if (myApi.getParagraphText(i).length() > 0) {
+					myParagraphIndex = i;
+					break;
+				}
+			}
+			if (myApi.getPageStart().ParagraphIndex >= myParagraphIndex) {
+				myApi.setPageStart(new TextPosition(myParagraphIndex, 0, 0));
+			}
+			highlightParagraph();
+			runOnUiThread(new Runnable() {
+				public void run() {
+					findViewById(R.id.button_next_paragraph).setEnabled(true);
+					findViewById(R.id.button_play).setEnabled(true);
+				}
+			});
+		} catch (ApiException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String gotoNextParagraph() {
+		try {
+			String text = "";
+			for (; myParagraphIndex < myParagraphsNumber; ++myParagraphIndex) {
+				final String s = myApi.getParagraphText(myParagraphIndex);
+				if (s.length() > 0) {
+					text = s;
+					break;
+				}
+			}
+			if (!"".equals(text) && !myApi.isPageEndOfText()) {
+				myApi.setPageStart(new TextPosition(myParagraphIndex, 0, 0));
+			}
+			highlightParagraph();
+			if (myParagraphIndex >= myParagraphsNumber) {
+				runOnUiThread(new Runnable() {
+					public void run() {
+						findViewById(R.id.button_next_paragraph).setEnabled(false);
+						findViewById(R.id.button_play).setEnabled(false);
+					}
+				});
+			}
+			return text;
+		} catch (ApiException e) {
+			e.printStackTrace();
+			return "";
 		}
 	}
 }
