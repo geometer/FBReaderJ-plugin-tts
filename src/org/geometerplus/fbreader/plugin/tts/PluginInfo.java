7463f3fa93bcb11e103b882e22f91665d9b87150
Nikolay Pultsin <geometer@fbreader.org>
2011-06-24 10:13:35 +0100

    API synchronization

--- src/org/geometerplus/fbreader/plugin/tts/PluginInfo.java
+++ src/org/geometerplus/fbreader/plugin/tts/PluginInfo.java
@@ -29,7 +29,7 @@ import org.geometerplus.android.fbreader.api.PluginApi;
 public class PluginInfo extends PluginApi.PluginInfo {
 	@Override
 	protected List<PluginApi.ActionInfo> implementedActions(Context context) {
-		return Collections.singletonList(new PluginApi.ActionInfo(
+		return Collections.<PluginApi.ActionInfo>singletonList(new PluginApi.MenuActionInfo(
 			Uri.parse("http://data.fbreader.org/plugin/tts/speak"),
 			context.getText(R.string.speak_menu_item).toString()
 		));
